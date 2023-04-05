17e41a8000d61b2303118eb7ec141c768e8728ee
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-05 17:45:49 -0400

    dev port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Config
+const port = 3000
+
 // Import Dependencies
 
 // Create
 
 // Delete (unsubscribe)
 
-
+app.listen(port, () => { console.log('Server is running on port 3000')});
 
