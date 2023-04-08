36169c6b22bc390e883bcdfdc230d38e8f3f8b93
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-08 17:16:25 -0400

    installed SwiperJS

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useRef, useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
 
-export default function Carousel() {
+register();
+
+export const Carousel = () => {
+  const swiperElRef = useRef(null);
+
+  useEffect(() => {
+    // listen for Swiper events using addEventListener
+    swiperElRef.current.addEventListener('progress', (e) => {
+      const [swiper, progress] = e.detail;
+      console.log(progress);
+    });
+
+    swiperElRef.current.addEventListener('slidechange', (e) => {
+      console.log('slide changed');
+    });
+  }, []);
+
   return (
-    <div>
-        // image carousel with swiper.js
-    </div>
-  )
-}
+    <swiper-container
+      ref={swiperElRef}
+      slides-per-view="1"
+      navigation="true"
+      pagination="true"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper-container>
+  );
+};
