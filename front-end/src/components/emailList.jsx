8e47ecdf786bafe2499fba4e76ac8ec83c405215
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-08 21:58:46 -0400

    installed yup and added formik form

--- front-end/src/components/emailList.jsx
+++ front-end/src/components/emailList.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 const axios = require('axios');
 
 
@@ -9,7 +10,41 @@ const submitEmail = (e) => {
   const email = e.target.value;
   const formatEmail = email.toLowerCase().trim()
 }
+// yup
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
+  onSubmit: values => {
+    alert(JSON.stringify(values, null, 2));
+  },
+});}
 
+
+<Formik
+       initialValues={{ email: ''}}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     ></Formik>
 
 export default function EmailList() {
   return (
@@ -18,9 +53,17 @@ export default function EmailList() {
         // small unsubscribe link
         // paragraph about signing up for emails */}
         <form action="">
-          <input value="email" type="email" name="userEmail" id="email" />
+          <label htmlFor="email">Email Address</label>
+          <input value="email" type="email" name="userEmail" id="email" 
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email} />
+
+          {formik.touched.email && formik.errors.email ? (
+         <div>{formik.errors.email}</div>) : null}
           <button type='submit'>Subscribe</button>
         </form>
+        
     </div>
   )
 }
