84ebbe9672ffcccb768489e5c9e7f2584abb27c8
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-10 06:52:22 -0400

    debug email submission

--- front-end/src/components/emailList.jsx
+++ front-end/src/components/emailList.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { useFormik } from 'formik';
-//import { Formik } from 'formik/dist/formik.cjs.development';
 import { Formik } from 'formik';
 import { emailSchema } from '../../validations/EmailValidation';
-import * as Yup from 'yup';
 
-const onSubmit = async (values, actions) => {
-  console.log(values);
-  console.log(actions);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  alert(JSON.stringify(value, null, 2));
-  actions.resetForm();
-}
 export default function EmailForm() {
   
-  const { values, errors, isSubmitting, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+  const onSubmit = async (value, actions) => {
+    console.log(value);
+    console.log(actions);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    alert(JSON.stringify(values, null, 2));
+    actions.resetForm();
+  }
+  
+  const { values, isSubmitting, handleBlur, handleChange, handleSubmit } = useFormik({
   initialValues: {
     email: '',
   },
@@ -23,14 +22,14 @@ export default function EmailForm() {
   return (
     <div className="m-2">
       <Formik>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={onSubmit}>
           <label className='mx-2' htmlFor="email">Join Our Email List</label>
           <input
             id="email"
             name="email"
             type="email"
             onChange={handleChange}
-            value={values.email}
+            values={values.email}
             placeholder='Enter your email'
             onBlur={handleBlur}
             className='mx-2 p-1 rounded-lg text-center'
