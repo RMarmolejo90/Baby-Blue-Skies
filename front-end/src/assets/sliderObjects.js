9045281eb2eb674aa69bf962b59a701a0633fda2
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-13 15:01:55 -0400

    added id to objects

--- front-end/src/assets/sliderObjects.js
+++ front-end/src/assets/sliderObjects.js
@@ -1,13 +1,15 @@
-const Images = [
-    {
+const sliderImages = [
+    {   id:"1",
         location:"Athens, Greece",
         image:"front-end/src/assets/carousel/Athens-greece.jpg"
     },
     {
+        id:"2",
         location:"Indonesia",
         image:"front-end/src/assets/carousel/Banten-Indonesia.jpg"
     },
     {
+        id:"3",
         location:"Tanzania",
         image:"front-end/src/assets/carousel/Arusha-Region-Tanzania.jpg"
     },
@@ -16,222 +18,273 @@ const Images = [
         image:"front-end/src/assets/carousel/Bayan-Ölgii-Mongolia.jpg"
     },
     {
+        id:"4",
         location:"Bermuda",
         image:"front-end/src/assets/carousel/bermuda.jpg"
     },
     {
+        id:"5",
         location:"Bordeaux, France",
         image:"front-end/src/assets/carousel/Bordeaux-France.jpg"
     },
     {
+        id:"6",
         location:"Buenos Aires, Argentina",
         image:"front-end/src/assets/carousel/Buenos-Aires-Argentina.jpg"
     },
     {
+        id:"7",
         location:"Caraga, Philippines",
         image:"front-end/src/assets/carousel/Caraga-Philippines.jpg"
     },
-    {
+    {id:"8",
         location:"Central Province, Maldives",
         image:"front-end/src/assets/carousel/Central-Province-Maldives.jpg"
     },
     {
+        id:"9",
         location:"Honduras",
         image:"front-end/src/assets/carousel/Coxen Hole-honduras.jpg"
     },
     {
+        id:"10",
         location:"Cusco, Peru",
         image:"front-end/src/assets/carousel/Cusco-Peru-2.jpg"
     },
     {
+        id:"11",
         location: "Dubai, United Arab Emirates",
         image:"front-end/src/assets/carousel/Dubai-United-Arab-Emirates.jpg"
     },
     {
+        id:"12",
         location:"Algeria",
         image:"front-end/src/assets/carousel/Djanet-Illizi-Province-Algeria.jpg"
     },
     {
+        id:"13",
         location:"Cusco, Peru",
         image:"front-end/src/assets/carousel/Cusco-Peru.jpg"
     },
     {
+        id:"14",
         location:"Dunamaise, Ireland",
         image:"front-end/src/assets/carousel/Dunamaise-ireland.jpg"
     },
     {
+        id:"15",
         location:"Egypt",
         image:"front-end/src/assets/carousel/egypt.jpg"
     },
     {
+        id:"16",
         location:"Etratat, France",
         image:"front-end/src/assets/carousel/Étretat-france.jpg"
     },
     {
+        id:"17",
         location:"French Polynesia",
         image:"front-end/src/assets/carousel/French-Polynesia.jpg"
     },
     {
+        id:"18",
         location:"Germany",
         image:"front-end/src/assets/carousel/germany-castle.jpg"
     },
     {
+        id:"19",
         location:"Gramado, Brazil",
         image:"front-end/src/assets/carousel/Gramado-Brazil.jpg"
     },
     {
+        id:"20",
         location:"Sri Lanka",
         image:"front-end/src/assets/carousel/Hambantota-Sri Lanka.jpg"
     },
     {
+        id:"21",
         location:"Iceland",
         image:"front-end/src/assets/carousel/iceland.jpg"
     },
     {
+        id:"22",
         location:"India",
         image:"front-end/src/assets/carousel/india.jpg"
     },
     {
+        id:"23",
         location:"Indonesia",
         image:"front-end/src/assets/carousel/indonesia.jpg"
     },
     {
+        id:"24",
         location:"Ireland",
         image:"front-end/src/assets/carousel/ireland.jpg"
     },
     {
+        id:"25",
         location:"Kajiado County, Kenya",
         image:"front-end/src/assets/carousel/Kajiado-County-Kenya.jpg"
     },
     {
+        id:"26",
         location:"Dominican Republic",
         image:"front-end/src/assets/carousel/La-Altagracia-República-Dominicana.jpg"
     },
     {
+        id:"27",
         location:"Limon, Costa Rica",
         image:"front-end/src/assets/carousel/Limón-Costa-Rica.jpg"
     },
     {
+        id:"28",
         location:"Luang Prabang Province, Laos",
         image:"front-end/src/assets/carousel/Luang-Prabang-Province-Laos.jpg"
     },
     {
+        id:"29",
         location:"Maafushi, Maldives",
         image:"front-end/src/assets/carousel/Maafushi-maldives.jpg"
     },
     {
+        id:"30",
         location:"Maldives",
         image:"front-end/src/assets/carousel/maldives1.jpg"
     },
     {
+        id:"31",
         location:"Male, Maldives",
         image:"front-end/src/assets/carousel/Male-Maldives.jpg"
     },
     {
+        id:"32",
         location:"Moraine Lake, Canada",
         image:"front-end/src/assets/carousel/moraine-lake-canada.jpg"
     },
     {
+        id:"33",
         location:"Mount Cook, New Zealand",
         image:"front-end/src/assets/carousel/mount-cook-new-zealand.jpg"
     },
     {
+        id:"34",
         location:"Mueang Kao, Thailand",
         image:"front-end/src/assets/carousel/Mueang Kao-thailand.jpg"
     },
     {
+        id:"35",
         location:"Myrland Nordland, Norway",
         image:"front-end/src/assets/carousel/Myrland-Nordland-Norway.jpg"
     },
     {
+        id:"36",
         location:"Nassau Nova Providencia, Bahamas",
         image:"front-end/src/assets/carousel/Nassau-Nova-Providência-Bahamas.jpg"
     },
     {
+        id:"37",
         location:"Paris, France",
         image:"front-end/src/assets/carousel/paris-france.jpg"
     },
     {
+        id:"38",
         location: "Pattaya City, Thailand",
         image:"front-end/src/assets/carousel/Pattaya-City-thailand.jpg"
     },
     {
+        id:"39",
         location:"Rio De Janeiro, Brazil",
         image:"front-end/src/assets/carousel/rio-de-janeiro-brazil.jpg"
     },
     {
+        id:"40",
         location:"Rome, Italy",
         image:"front-end/src/assets/carousel/Rome Colloseum.jpg"
     },
     {
+        id:"41",
         location:"San-Juan, Bautista",
         image:"front-end/src/assets/carousel/San-Juan-Bautista.jpg"
     },
     {
+        id:"42",
         location:"Santarem District, Portugal",
         image:"front-end/src/assets/carousel/Santarém-District-Portugal.jpg"
     },
     {
+        id:"43",
         location:"Santorini, Greece",
         image:"front-end/src/assets/carousel/Santorini-Greece.jpg"
     },
     {
+        id:"44",
         location:"Siem Reap Province, Cambodia",
         image:"front-end/src/assets/carousel/Siem-Reap-Province-Cambodia.jpg"
     },
     {
+        id:"45",
         location:"Singapore",
         image:"front-end/src/assets/carousel/singapore-1.jpg"
     },
 
     {
+        id:"46",
         location:"Soledad de Graciano, Mexico",
         image:"front-end/src/assets/carousel/Soledad-de-Graciano-Sánchez-Mexico.jpg"
     },
 
     {
+        id:"47",
         location:"Tangier, Morocco",
         image:"front-end/src/assets/carousel/Tangier-morocco.jpg"
     },
 
     {
+        id:"48",
         location:"Thailand",
         image:"front-end/src/assets/carousel/thailand2.jpg"
     },
 
     {
+        id:"49",
         location:"Singapore",
         image:"front-end/src/assets/carousel/singapore.jpg"
     },
 
     {
+        id:"50",
         location:"Tokyo, Japan",
         image:"front-end/src/assets/carousel/tokyo-japan-1.jpg"
     },
 
     {
+        id:"51",
         location:"Trentino, Italia",
         image:"front-end/src/assets/carousel/Trentino-Alto-Adige-Italia.jpg",
     },
 
     {
+        id:"52",
         location:"Venice, Italy",
         image:"front-end/src/assets/carousel/Venice-Veneto-Italy.jpg",
     },
     {
+        id:"53",
         location:"Wicklow, Ireland",
         image:"front-end/src/assets/carousel/Wicklow-Ireland.jpg",
     },
 
     {
+        id:"54",
         location:"Tokyo, Japan",
         image:"front-end/src/assets/carousel/tokyo-japan.jpg",
     },
 
     {
+        id:"55",
         location:"Zakopane, Poland",
         image:"front-end/src/assets/carousel/Zakopane-poland.jpg",
     }
 ]
 
-export default Images
+export default sliderImages;
--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -1,14 +1,13 @@
 // import Swiper core and required modules
 import { Navigation, A11y } from 'swiper';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
+import sliderImages from '../assets/sliderObjects.js';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 
 //content
-import {Images} from '../assets/sliderObjects'
 
 export default () => {
   return (
@@ -18,16 +17,16 @@ export default () => {
       spaceBetween={50}
       slidesPerView={1}
       navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide><img src="../assets/carousel/Arusha-Region-Tanzania.jpg" alt="Tanzania" /></SwiperSlide>
-      <SwiperSlide><img src="../assets/carousel/Athens-greece.jpg" alt="greece" /></SwiperSlide>
-      <SwiperSlide><img src="../assets/carousel/Banten-Indonesia.jpg" alt="indonesia" /></SwiperSlide>
-      <SwiperSlide><img src="../assets/carousel/Bayan-Ölgii-Mongolia.jpg" alt="mongolia" /></SwiperSlide>
-      ...
+      {sliderImages.map( sliderImages => (
+        <SwiperSlide key={sliderImages.id}>
+          <div>
+            {sliderImages.location}
+          </div>
+        </SwiperSlide>
+      ) )}
     </Swiper>
   );
 };
