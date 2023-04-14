0bd07b0a1487e8105fcce4f24a7e020f95e894e3
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-13 23:39:59 -0400

    replacing images with webP

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -1,11 +1,12 @@
 // import Swiper core and required modules
-import { Navigation, A11y } from 'swiper';
+import { Navigation, A11y, EffectFlip } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import sliderImages from '../assets/sliderObjects.js';
-
 // Import Swiper styles
-import 'swiper/css';
+import 'swiper/css/bundle';
 import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-flip';
 
 //content
 
@@ -13,17 +14,22 @@ export default () => {
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, A11y]}
+      modules={[Navigation, A11y, EffectFlip]}
+      enabled='true'
+      effect='flip'
+      loop='true'
       spaceBetween={50}
       slidesPerView={1}
       navigation
+      autoplay      
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
       {sliderImages.map( (sliderImages, index) => (
         <SwiperSlide key={index} className='slide'>
-          <div className='slide-wrapper'>
-            <img className='slide-image' src={sliderImages.image} alt={sliderImages.location} />
+          <div className='slide-wrapper '>
+            <div className="swiper-lazy-preloader"></div>
+            <img className='slide-image' loading='lazy' src={sliderImages.image} alt={sliderImages.location} />
             <div>
               <h2>{sliderImages.location}</h2>
             </div>
