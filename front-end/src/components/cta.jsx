d58c43d3a40c9e4e9f78e7cf6a333c741a7d4b99
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-16 19:22:42 -0400

    cta and hook styling

--- front-end/src/components/cta.jsx
+++ front-end/src/components/cta.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Cta() {
   return (
-    <div className='p-8 text-center flex items-center flex-col justify-center'>
+    <div className='p-8 text-center flex items-center flex-col justify-center text-lg'>
         {/* // image */}
-        <h1 className='p-4 m-4' >Tired of staring at the same four walls?</h1>
+        <h1 className='p-4 m-4 text-2xl' >Tired of staring at the same four walls?</h1>
         <h4 className='p-4 m-4' >
           Let us help you plan your next big adventure! Our expert travel agents will guide you every step of the way and make sure your trip is unforgettable. 
           Don't let your wanderlust go to waste - contact us today and let's start planning your dream escape!
         </h4>
-        <a className='p-2 m-4 text-sm bg-green-400 font-semibold border rounded-xl' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
+        <a className='p-2 px-10 m-4 text-sm bg-green-400 font-semibold border rounded-xl' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
             Start A New Trip
         </a>
     </div>
--- front-end/src/components/hook.jsx
+++ front-end/src/components/hook.jsx
@@ -3,23 +3,22 @@ import React from 'react'
 export default function Hook() {
   return (
     <div className='p-8 md:flex md:flex-row md:flex-wrap md:items-center justify-center'>
-        <div className='' >
-            <div className='p4'>
-                <h2 className='p-4 text-xl text-primary-dark'>Know what's better than taking a vacation? </h2>
-                <h4 className='p-4 text-xl'>
-                    Taking a vacation that doesn't 
-                    make you want to rip your hair out. 
-                </h4>
-            </div>
-            <img className='max-w-2xl md:max-w-md ' src="../src/assets/images/frustrated.jpg" alt="frustrated man sitting on a beach" />
+        <div className='' >    
+            <img className='max-w-2xl md:max-w-md pr-4' src="../src/assets/images/frustrated.jpg" alt="frustrated man sitting on a beach" />
         </div>
-        <h4 className='p-4 md:max-w-md'>
-            Let Baby Blue Skies Travel handle the details 
-            so you can focus on the fun stuff - like deciding what swimsuit to pack.
-            Do you know what's better than taking a vacation? Taking a vacation that doesn't make you want to rip your hair out. Let Baby Blue Skies Travel handle the 
-            details so you can focus on the fun stuff - like deciding what swimsuit to pack.        
-        </h4>
-        
+        <div className='pl-4'>
+            <h2 className='p-2 text-xl text-primary-dark'>Know what's better than taking a vacation? </h2>
+            <h4 className='p-2 text-xl'>
+                Taking a vacation that doesn't 
+                make you want to rip your hair out. 
+            </h4>
+            <h4 className='p-4 md:max-w-md'>
+                Let Baby Blue Skies Travel handle the details 
+                so you can focus on the fun stuff - like deciding what swimsuit to pack.
+                Do you know what's better than taking a vacation? Taking a vacation that doesn't make you want to rip your hair out. Let Baby Blue Skies Travel handle the 
+                details so you can focus on the fun stuff - like deciding what swimsuit to pack.        
+            </h4>
+        </div>
     </div>
   )
 }
