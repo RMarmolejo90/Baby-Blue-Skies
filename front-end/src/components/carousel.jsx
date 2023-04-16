35f2657ac258e0e3938a6235d190de71f1cd64c8
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-16 19:18:08 -0400

    added footer, changed some styles

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -1,12 +1,12 @@
 // import Swiper core and required modules
-import { Navigation, EffectCreative, Autoplay } from 'swiper';
+import { Navigation, EffectCube, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import sliderImages from '../assets/sliderObjects.js';
 // Import Swiper styles
 import 'swiper/css/bundle';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
-import 'swiper/css/effect-creative';
+import 'swiper/css/effect-cube';
 
 //content
 
@@ -16,23 +16,23 @@ export default () => {
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, EffectCreative, Autoplay]}
+      modules={[Navigation, EffectCube, Autoplay]}
       enabled='true'
       autoheight='true'
       autoplay={true}
-      effect='creative'
+      effect='cube'
       loop='true'
       lazyPreloadPrevNext={2}
       spaceBetween={50}
       slidesPerView={1}
       navigation   
       onSlideChange={() => console.log('slide change')}
-      className="lg:max-w-4xl shadow-3xl rounded-xl contrast-125 "
+      className="lg:max-w-2xl shadow-2xl contrast-125 "
     >
       {sliderImages.map( (sliderImages, index) => (
         <SwiperSlide key={index} className='slide'  >
           <div className='slide-wrapper'>
-            <img className='swiper-image w-full' loading="lazy" src={sliderImages.image} alt={sliderImages.location} />
+            <img className='swiper-image rounded-lg lg:min-h-max' loading="lazy" src={sliderImages.image} alt={sliderImages.location} />
             <div className="swiper-lazy-preloader"></div>
             {/* <div className=' relative  '>
               <h2 >{sliderImages.location}</h2>
--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <div className='bg-dark-shade mt-24' >
         {/* sitemap
         contact info */}
+        <div className='flex items-center justify-center text-primary-light'>
+          <h4 className='p-16'>BABY BLUE SKIES TRAVEL AGENCY</h4>
+        </div>
+
     </div>
   )
 }
