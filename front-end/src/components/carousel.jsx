91231cfc5d81bea1b5e43bd8a106f38f60592dd7
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-16 16:18:57 -0400

    changed swiper effect to creative

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -1,12 +1,12 @@
 // import Swiper core and required modules
-import { Navigation, EffectFade, Autoplay } from 'swiper';
+import { Navigation, EffectCreative, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import sliderImages from '../assets/sliderObjects.js';
 // Import Swiper styles
 import 'swiper/css/bundle';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
-import 'swiper/css/effect-flip';
+import 'swiper/css/effect-creative';
 
 //content
 
@@ -16,22 +16,23 @@ export default () => {
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, EffectFade, Autoplay]}
+      modules={[Navigation, EffectCreative, Autoplay]}
       enabled='true'
+      autoheight='true'
       autoplay={true}
-      effect='fade'
+      effect='creative'
       loop='true'
       lazyPreloadPrevNext={2}
       spaceBetween={50}
       slidesPerView={1}
       navigation   
       onSlideChange={() => console.log('slide change')}
-      className="md:max-w-6xl shadow-3xl rounded-xl contrast-125 "
+      className="lg:max-w-4xl shadow-3xl rounded-xl contrast-125 "
     >
       {sliderImages.map( (sliderImages, index) => (
         <SwiperSlide key={index} className='slide'  >
           <div className='slide-wrapper'>
-            <img className='swiper-image' loading="lazy" src={sliderImages.image} alt={sliderImages.location} />
+            <img className='swiper-image w-full' loading="lazy" src={sliderImages.image} alt={sliderImages.location} />
             <div className="swiper-lazy-preloader"></div>
             {/* <div className=' relative  '>
               <h2 >{sliderImages.location}</h2>
--- front-end/src/components/hook.jsx
+++ front-end/src/components/hook.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 export default function Hook() {
   return (
-    <div className='p-8'>
-        <h2 className='p-4'>Know what's better than taking a vacation? </h2>
-        <h4 className='p-4'>
-            Taking a vacation that doesn't 
-            make you want to rip your hair out. 
-        </h4>
-        <img className='max-w-full' src="../assets/images/frustrated.jpg" alt="frustrated man sitting on a beach" />
-        <h4>
+    <div className='p-8 md:flex md:flex-row md:flex-wrap md:items-center justify-center'>
+        <div className='' >
+            <div className='p4'>
+                <h2 className='p-4 text-xl text-primary-dark'>Know what's better than taking a vacation? </h2>
+                <h4 className='p-4 text-xl'>
+                    Taking a vacation that doesn't 
+                    make you want to rip your hair out. 
+                </h4>
+            </div>
+            <img className='max-w-2xl md:max-w-md ' src="../src/assets/images/frustrated.jpg" alt="frustrated man sitting on a beach" />
+        </div>
+        <h4 className='p-4 md:max-w-md'>
             Let Baby Blue Skies Travel handle the details 
             so you can focus on the fun stuff - like deciding what swimsuit to pack.
             Do you know what's better than taking a vacation? Taking a vacation that doesn't make you want to rip your hair out. Let Baby Blue Skies Travel handle the 
