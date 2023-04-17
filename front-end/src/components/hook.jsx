bfdebba8462d39bf71e36af0fb54bad7570a2145
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-17 07:28:09 -0400

    responsive design - hook

--- front-end/src/components/hook.jsx
+++ front-end/src/components/hook.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 
 export default function Hook() {
   return (
-    <div className='p-8 md:flex md:flex-row md:flex-wrap md:items-center justify-center'>
-        <div className='' >    
-            <img className='max-w-2xl md:max-w-md pr-4' src="../src/assets/images/frustrated.jpg" alt="frustrated man sitting on a beach" />
+    <div className='lg:p-8 flex flex-col lg:flex-row flex-wrap items-center justify-center'>
+        <div className='max-w-md lg:max-w-lg' >    
+            <h2 className='p-4 lg:max-w-md text-xl text-primary-dark font-semibold'>Know what's better than taking a vacation? </h2>
+            <img className='max-w-sm lg:max-w-lg lg:pr-4 overflow-hidden' src="../src/assets/images/frustrated.jpg" alt="frustrated man sitting on a beach" />
         </div>
-        <div className='pl-4'>
-            <h2 className='p-2 text-xl text-primary-dark'>Know what's better than taking a vacation? </h2>
-            <h4 className='p-2 text-xl'>
+        <div className='lg:pl-4 py-4'>
+            <h4 className='p-4 lg:max-w-md text-xl font-semibold'>
                 Taking a vacation that doesn't 
                 make you want to rip your hair out. 
             </h4>
-            <h4 className='p-4 md:max-w-md'>
+            <h4 className='p-4 lg:max-w-md'>
                 Let Baby Blue Skies Travel handle the details 
-                so you can focus on the fun stuff - like deciding what swimsuit to pack.
-                Do you know what's better than taking a vacation? Taking a vacation that doesn't make you want to rip your hair out. Let Baby Blue Skies Travel handle the 
-                details so you can focus on the fun stuff - like deciding what swimsuit to pack.        
+                so you can focus on the fun stuff - like deciding what swimsuit to pack.       
             </h4>
         </div>
     </div>
