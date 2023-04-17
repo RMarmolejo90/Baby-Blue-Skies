2dfcc5e7d7b011370721b08dfb56971c4af88000
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-17 07:09:24 -0400

    text unsubscribe

--- front-end/src/pages/unsubscribe.jsx
+++ front-end/src/pages/unsubscribe.jsx
@@ -36,16 +36,16 @@ const EmailForm = () => {
   return (
     <div>
         <Navbar />
-            <div className='flex items-center justify-center p-8'>
-                <h3>Unsubrcribe From Emails</h3>
-                <p>We're sorry to see you go and we hope that you've enjoyed receiving our special travel deals.
+            <div className='flex flex-col items-center justify-center p-8'>
+                <h3 className='font-bold text-xl text-primary-dark p-4'>Unsubscribe From Emails</h3>
+                <p className='py-4 px-12'>We're sorry to see you go and we hope that you've enjoyed receiving our special travel deals.
                  If you would like to unsubscribe from our emails, please fill out the form below with your email address and click the "unsubscribe" button.
                 We understand that everyone's travel preferences are unique and we hope that we were able to inspire you with our travel offers. 
                 If there's anything we could have done better or if you have any feedback, please don't hesitate to let us know! We're always looking for ways to improve our service and your input is valuable to us.
                 </p>
-                <p>If you have any questions or concerns about your future travel plans, our team is here to assist you. You can contact us using the information provided on this page and we'll be happy to help.</p>
+                <p className='py-4 px-12'>If you have any questions or concerns about your future travel plans, our team is here to assist you. You can contact us using the information provided on this page and we'll be happy to help.</p>
 
-                <p>Thank you for considering us as your travel agency and we wish you all the best on your future journeys!</p>
+                <p className='py-10 px-12'>Thank you for considering us as your travel agency and we wish you all the best on your future journeys!</p>
                 <form action='unsubscribe' onSubmit={formik.handleSubmit}>
                     <input
                     className='border-2 rounded-lg px-4 mx-4'
