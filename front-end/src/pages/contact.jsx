206489e1b320990c85ca933981496ce433d0ecd7
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-18 16:44:00 -0400

    finished contact page

--- front-end/src/pages/contact.jsx
+++ front-end/src/pages/contact.jsx
@@ -11,19 +11,22 @@ export default function Contact() {
   return (
     <div>
         <Navbar />
-        <h3 className='text-center py-6 bg-dark-shade text-4xl text-primary-light font-bold'>Contact Us</h3>
         <div className='px-20 py-10'>
+            <h3 className='text-center text-4xl text-primary-dark font-bold'>Contact Us</h3>
             <p className='py-10'>We're thrilled that you're considering reaching out to us! 
-            Our team is dedicated to helping you plan the perfect getaway and creating unforgettable experiences. Whether you need advice on choosing a destination, have questions about our services, or simply want to chat about all things travel, we're here to help. So drop us a line, and let's start planning your next adventure together! With our range of exciting activities and personalized recommendations,
+            Our team is dedicated to helping you plan the perfect getaway and creating unforgettable experiences. 
+            </p>
+            <p className='pb-10'>
+            Whether you need advice on choosing a destination, have questions about our services, or simply want to chat about all things travel, we're here to help. So drop us a line, and let's start planning your next adventure together! With our range of exciting activities and personalized recommendations,
             we're sure we can make your trip unforgettable.
             </p>
             <div className='flex flex-col items-center justify-between'>
                 <ul className='text-primary-dark flex flex-col items-between justify-center text-center text-xl'>
-                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><FontAwesomeIcon icon={faPhone} /><a href="tel:+13179366685">+1(317)936-6685</a></li>
-                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><FontAwesomeIcon icon={faEnvelope} /> BabyBlueSkiesTravel@gmail.com</li>
-                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><FontAwesomeIcon icon={faFacebookF} /></li>
-                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><FontAwesomeIcon icon={faInstagram} /></li>
-                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><FontAwesomeIcon icon={faTiktok} /></li>
+                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><a href="tel:+13179366685"><FontAwesomeIcon icon={faPhone} />+1(317)936-6685</a></li>
+                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><a href="mailto:babyblueskiestravel@gmail.com"><FontAwesomeIcon icon={faEnvelope} /> BabyBlueSkiesTravel@gmail.com</a></li>
+                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><a href="https://www.facebook.com/babyblueskiestravelag" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faFacebookF} /></a></li>
+                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><a href="https://www.instagram.com/babyblueskiestravel/" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faInstagram} /></a></li>
+                    <li className='py-6 px-2 hover:border-b hover:cursor-pointer hover:border-primary-dark'><a href="https://www.tiktok.com/@babyblueskiestravel" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faTiktok} /></a></li>
                 </ul>
             </div>
         </div>
