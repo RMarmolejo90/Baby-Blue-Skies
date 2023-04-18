0f73a560d1d5c400d96de08a848ebe9de3255358
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-18 05:40:08 -0400

    ready to add font awesome icon

--- front-end/src/components/navbar.jsx
+++ front-end/src/components/navbar.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const [toggleMenu, setToggleMenu] = useState('inactive');
+
+const handleMenuToggle = () => {
+  toggleMenu === 'active' ? setToggleMenu('inactive') : setToggleMenu('active');
+}
 
 export default function Navbar() {
   return (
@@ -8,10 +15,10 @@ export default function Navbar() {
         <h2 className='m-2'>Baby Blue Skies Travel</h2>
         <div>
           <ul>
-            <li><a href="/unsubscribe">unsubscribe</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/">Home</a></li>
+            <li><Link to='/about'>About</Link> </li>
+            <li><Link to='/'>Home</Link></li>
             <li><a href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform" target="_blank" rel="noopener noreferrer">Start a New Trip</a></li>
+            <li><Link to='/contact'>Contact Us</Link></li>
           </ul>
         </div>
 
