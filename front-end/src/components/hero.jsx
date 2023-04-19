b5bc484d71e5d6652cfd4820169a700af02ca009
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-19 00:48:15 -0400

    hero edits

--- front-end/src/components/hero.jsx
+++ front-end/src/components/hero.jsx
@@ -5,14 +5,14 @@ export default function Hero() {
   return (
     <div>
         <div className='pb-20 p-10 flex flex-col flex-wrap items-center justify-center min-h-[70vh] bg-[url(../src/assets/images/beach-landscape.jpg)] bg-cover bg-no-repeat'>
-            <Fade left delay={1500} duration={1500}>
-                <div className='shadow-inner text-center p-6 flex flex-col flex-wrap items-center justify-center mt-10 rounded-xl border'> 
-                    <h2 className='my-4 text-3xl font-bold'>
+            <Fade delay={1000} duration={2000}>
+                <div className='shadow-inner text-center p-6 pb-10 flex flex-col flex-wrap items-center justify-center mt-10 rounded-xl border'> 
+                    <h2 className='pb-8 m-4 text-3xl font-bold'>
                         Let's Start Planning Your Dream Vacation!
                     </h2>
                     {/* cta button */}
                     <JackInTheBox delay={1500} duration={2000}>
-                        <a className='p-3 m-4 tracking-wider leading-6 text-md text-center bg-green-500 font-bold border rounded-md' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
+                        <a className='p-3 mx-4 my-8 tracking-wider leading-6 text-md text-center bg-green-500 font-bold border rounded-md' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
                             Start A Trip Request
                     </a>
                     </JackInTheBox>
