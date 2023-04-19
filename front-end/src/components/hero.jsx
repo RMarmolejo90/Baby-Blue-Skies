1428d78bc3f42c2f340125bfa3e5906a50e523aa
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-19 00:34:51 -0400

    finished hero styling

--- front-end/src/components/hero.jsx
+++ front-end/src/components/hero.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Fade } from 'react-awesome-reveal'; 
+import { Fade, Slide, JackInTheBox } from 'react-awesome-reveal';
 
 export default function Hero() {
   return (
     <div>
         <div className='pb-20 p-10 flex flex-col flex-wrap items-center justify-center min-h-[70vh] bg-[url(../src/assets/images/beach-landscape.jpg)] bg-cover bg-no-repeat'>
-            <div className='shadow-inner text-center p-4 flex flex-col flex-wrap items-center justify-center mt-10 rounded-xl border backdrop-blur-lg backdrop-brightness-150'> 
-                <h2 className='my-4 text-3xl font-semibold'>
-                    Let's Start Planning Your Dream Vacation!
-                </h2>
-                {/* cta button */}
-                <a className='p-2 m-4 tracking-wider leading-6 text-sm text-center bg-green-500 font-semibold border rounded-md' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
-                    Start A Trip Request
-                </a>
-            </div>
+            <Fade left delay={1500} duration={1500}>
+                <div className='shadow-inner text-center p-6 flex flex-col flex-wrap items-center justify-center mt-10 rounded-xl border'> 
+                    <h2 className='my-4 text-3xl font-bold'>
+                        Let's Start Planning Your Dream Vacation!
+                    </h2>
+                    {/* cta button */}
+                    <JackInTheBox delay={1500} duration={2000}>
+                        <a className='p-3 m-4 tracking-wider leading-6 text-md text-center bg-green-500 font-bold border rounded-md' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
+                            Start A Trip Request
+                    </a>
+                    </JackInTheBox>
+                </div>
+            </Fade>
         </div>
     </div>
   )
