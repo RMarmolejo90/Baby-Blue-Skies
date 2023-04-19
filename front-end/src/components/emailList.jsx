0ddc842aaa247ac10d2672e82fd0f447a19011a6
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-19 00:44:43 -0400

    cleaned up email section

--- front-end/src/components/emailList.jsx
+++ front-end/src/components/emailList.jsx
@@ -32,13 +32,13 @@ const EmailForm = () => {
   });
 
   return (
-    <div className='flex flex-col items-center justify-center p-8 pb-20 bg-secondary-light'>
+    <div className='flex flex-col items-center justify-center p-20 bg-secondary-light'>
       <h3 className='text-primary-dark font-semibold text-xl p-4'>Want to stay up-to-date on the latest travel deals and offers?</h3>
       <p className='p-4'>Sign up for our email newsletter today!
         By subscribing to our email list, Be the first to know about the best travel deals! 
         Sign up for our newsletter to receive updates on the latest discounts and special offers.
       </p>
-      <p className='p-4'>
+      <p className='p-4 py-8'>
         We We promise not to overload your inbox with emails or share your information with third parties. 
         You can unsubscribe at any time by clicking the "unsubscribe" link at the bottom of our emails.
       </p>
