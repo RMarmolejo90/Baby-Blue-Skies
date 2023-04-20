3913715efc1020370d7ae962072c4ec4b5555917
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-19 21:54:25 -0400

    Revert "fix"
    This reverts commit 7a1eabd9bb10da81dea233f574fdb538022dd53d.

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'Baby-Blue-Skies/front-end/index.html'
+      input: 'path/to/index.html'
     }
   }
 }
