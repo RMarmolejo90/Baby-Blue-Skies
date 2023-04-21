c0ee9189c6dcb3b1e5e96653e807dc10ce3007b8
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-20 21:37:16 -0400

    deploy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,27 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-        input: [
-            './src/main.jsx',
-            './public/index.html'
-            ],
-        external: [
-            'react',
-            'react-dom',
-            'react-router-dom',
-            'Formik',
-            'yup',
-            'react-awesome-reveal',
-            '@fortawesome/react-fontawesome',
-            '@fortawesome/free-brands-svg-icons',
-            'swiper',
-            'axios',
-        ]
-    }
-    }
-})
+      input: ['./src/main.jsx'],
+      external: [
+        'react',
+        'react-dom',
+        'react-router-dom',
+        'Formik',
+        'yup',
+        'react-awesome-reveal',
+        '@fortawesome/react-fontawesome',
+        '@fortawesome/free-brands-svg-icons',
+        'swiper',
+        'axios',
+      ],
+    },
+  },
+});
