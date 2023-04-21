a667d30d1d6ef0c83284dfb0df98bf81d56a4199
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-21 07:10:33 -0400

    config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         rollupOptions: {
           input: {
             main: './index.html',
-          }
+          },
+          outDir: '../dist',
         }
       },
       
@@ -24,5 +25,5 @@ export default defineConfig({
         'swiper',
         'axios',
       ],
-    },
+  },
 );
