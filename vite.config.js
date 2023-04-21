44624253671cb1b620c5727b24628e7f94f04106
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-20 22:32:51 -0400

    deleting node modules next

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: ['./src/main.jsx'],
+      input: 
+        'front-end/public/index.html',
       external: [
         'react',
         'react-dom',
