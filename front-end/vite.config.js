b888c1cc8a54fc4df8d216b70b916a7d30d71899
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-21 06:57:34 -0400

    refresh

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 
-        'front-end/public/index.html',
+      build: {
+        rollupOptions: {
+          input: {
+            main: './index.html',
+          }
+        }
+      },
+      
       external: [
         'react',
         'react-dom',
@@ -21,5 +25,4 @@ export default defineConfig({
         'axios',
       ],
     },
-  },
-});
+);
