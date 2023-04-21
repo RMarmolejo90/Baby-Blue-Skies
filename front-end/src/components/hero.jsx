d2a86302b1b0f78d85a9b5c73fcb98f28b66909f
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-21 14:59:54 -0400

    quicker animations hero

--- front-end/src/components/hero.jsx
+++ front-end/src/components/hero.jsx
@@ -5,13 +5,13 @@ export default function Hero() {
   return (
     <div>
         <div className='pb-20 p-10 flex flex-col flex-wrap items-center justify-center min-h-[70vh] bg-[url(../src/assets/images/beach-landscape.jpg)] bg-cover bg-no-repeat'>
-            <Fade delay={1000} duration={2000}>
+            <Fade delay={200} duration={600}>
                 <div className='shadow-inner  text-center p-6 pb-10 flex flex-col flex-wrap items-center justify-center mt-10 rounded-xl border'> 
                     <h2 className='pb-8 m-4 text-3xl font-bold'>
                         Let's Start Planning Your Dream Vacation!
                     </h2>
                     {/* cta button */}
-                    <JackInTheBox delay={1500} duration={2000}>
+                    <JackInTheBox delay={500} duration={500}>
                         <a className='p-3 mx-4 my-8 tracking-wider leading-6 text-md text-center bg-button-color font-bold border rounded-md' href="https://docs.google.com/forms/d/e/1FAIpQLScvZkBlv69f74k6q7XJ9szgq_JoZR9Nhug3dsN67I06iQ5Neg/viewform">
                             Start A Trip Request
                     </a>
