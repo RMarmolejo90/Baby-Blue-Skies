626d3dd50f3828e2fcae03d7823be63c765fbb6d
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-20 21:26:18 -0400

    deploy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -6,22 +6,22 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input:
-        './src/main.jsx',
-        
-        
-      external: [
-        'react',
-        'react-dom',
-        'react-router-dom',
-        'Formik',
-        'yup',
-        'react-awesome-reveal',
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/free-brands-svg-icons',
-        'swiper',
-        'axios',
-      ]
+        input: [
+            './src/main.jsx',
+            './public/index.html'
+            ],
+        external: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'Formik',
+            'yup',
+            'react-awesome-reveal',
+            '@fortawesome/react-fontawesome',
+            '@fortawesome/free-brands-svg-icons',
+            'swiper',
+            'axios',
+        ]
     }
-  }
+    }
 })
