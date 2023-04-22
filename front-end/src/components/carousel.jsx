023d6c87a8bbc36d19eb8d3bdcefccd963a5962d
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-22 17:46:52 -0400

    build

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -7,7 +7,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import 'swiper/css/effect-cube';
 import { Fade } from 'react-awesome-reveal';
-import {Sliders} from './sliderObjects';
+import sliders from './sliderObjects';
 
 //content
 
@@ -32,13 +32,13 @@ export default () => {
           onSlideChange={() => console.log('slide change')}
           className="lg:max-w-xl shadow-2xl contrast-125"
         >
-          {Sliders.map( (Sliders, index) => (
+          {sliders.map( (sliders, index) => (
             <SwiperSlide key={index} className='slide'  >
               <div className='slide-wrapper'>
-              <img className='swiper-image rounded-lg lg:min-h-max' loading="lazy" src={Sliders.image} alt={Sliders.location} />
+              <img className='swiper-image rounded-lg lg:min-h-max' loading="lazy" src={sliders.image} alt={sliders.location} />
                 <div className="swiper-lazy-preloader"></div>
                 <div className=' relative mb-32 text-white-shade text-2xl'>
-                  <h2 >{Sliders.location}</h2>
+                  <h2 >{sliders.location}</h2>
                 </div> 
               </div>
             </SwiperSlide>
