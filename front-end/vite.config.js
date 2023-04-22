86f8f54df244b5db3162d6736bb22417f42fcdc8
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-22 12:53:45 -0400

    changed to main.jsx per vite docs

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
       build: {
+        manifest: true,
         rollupOptions: {
           input: {
-            main: './index.html',
+            main: './src/main.jsx',
           },
         },
 
