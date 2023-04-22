5c79b6cce9d0e508ada3474905ff825a04cc9534
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-22 15:11:59 -0400

    text minor changes

--- front-end/src/components/hook.jsx
+++ front-end/src/components/hook.jsx
@@ -4,14 +4,14 @@ import frustratedImage from '../assets/images/frustrated.jpg';
 
 export default function Hook() {
   return (
-    <div className='bg-primary-light py-24 lg:p-20 flex flex-col lg:flex-row flex-wrap items-center justify-center'>
+    <div className='bg-primary-light py-32 lg:p-20 flex flex-col lg:flex-row flex-wrap items-center justify-center'>
         <Slide duration={300}>
-            <div className='lg:max-w-lg flex flex-col items-center justify-center' >    
-                <img className='max-w-[100vw] xl:max-w-md lg:pr-4 lg:max-w-sm' src={frustratedImage} alt="frustrated man sitting on a beach" />
+            <div className='lg:max-w-lg ' >    
+                <img className='max-w-[100vw] xl:max-w-md lg:pr-6 lg:max-w-sm' src={frustratedImage} alt="frustrated man sitting on a beach" />
             </div>
-            <div className='lg:pl-4 py-4'>
-                <h2 className='p-4 w-full text-xl text-primary-dark font-semibold'>Know what's better than taking a vacation? </h2>
-                <h4 className='p-4 lg:max-w-sm text-xl'>
+            <div className='lg:pl-6 p-4 flex flex-col flex-wrap items-center justify-center'>
+                <h2 className='p-4 lg:max-w-sm text-2xl font-semibold'>Know what's better than taking a vacation? </h2>
+                <h4 className='p-4 lg:max-w-sm text-xl font-semibold'>
                     Taking a vacation that doesn't 
                     make you want to rip your hair out. 
                 </h4>
