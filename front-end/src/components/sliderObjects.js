53f0b1c4a9b38d71e4acc4754443a0b9ded766df
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-22 17:40:03 -0400

    moved dir

--- front-end/src/components/sliderObjects.js
+++ front-end/src/components/sliderObjects.js
@@ -1,26 +1,25 @@
-import Athens from '../assets/sliderImages/Athens-greece.jpeg';
-import Bermuda from '../assets/sliderImages/bermuda.jpeg';
-import Bordeaux from '../assets/sliderImages/Bordeaux-France.jpeg';
-import Maldives from '../assets/sliderImages/Central-Province-Maldives.jpeg';
-import iceland from '../assets/sliderImages/iceland.jpeg';
-import ireland from '../assets/sliderImages/ireland.jpeg';
-import Costa from '../assets/sliderImages/Limón-Costa-Rica.jpeg';
-import Laos from '../assets/sliderImages/Luang-Prabang-Province-Laos.jpeg';
-import Male from '../assets/sliderImages/Male-Maldives.jpeg';
-import Canada from '../assets/sliderImages/moraine-lake-canada.jpeg';
-import Norway from '../assets/sliderImages/Myrland-Nordland-Norway.jpeg';
-import Bahamas from '../assets/sliderImages/Nassau-Nova-Providência-Bahamas.jpeg';
-import Paris from '../assets/sliderImages/paris-france.jpeg';
-import Brazil from '../assets/sliderImages/rio-de-janeiro-brazil.jpeg';
-import Rome from '../assets/sliderImages/Rome-Colloseum.jpeg';
-import Bautista from '../assets/sliderImages/San-Juan-Bautista.jpeg';
-import Greece from '../assets/sliderImages/Santorini-Greece.jpeg';
-import Mexico from '../assets/sliderImages/Tangier-morocco.jpeg';
-import Singapore from '../assets/sliderImages/singapore.jpeg';
-import Tokyo from '../assets/sliderImages/tokyo-japan-1.jpeg';
-import Venice from '../assets/sliderImages/Venice-Veneto-Italy.jpeg';
-import Wicklow from '../assets/sliderImages/Wicklow-Ireland.jpeg';
-import Poland from '../assets/sliderImages/Zakopane-poland.jpeg';
+import Athens from '../assets/images/sliderImages/Athens-greece.jpeg';
+import Bermuda from '../assets/images/sliderImages/bermuda.jpeg';
+import Maldives from '../assets/images/sliderImages/Central-Province-Maldives.jpeg';
+import iceland from '../assets/images/sliderImages/iceland.jpeg';
+import ireland from '../assets/images/sliderImages/ireland.jpeg';
+import Costa from '../assets/images/sliderImages/Limón-Costa-Rica.jpeg';
+import Laos from '../assets/images/sliderImages/Luang-Prabang-Province-Laos.jpeg';
+import Male from '../assets/images/sliderImages/Male-Maldives.jpeg';
+import Canada from '../assets/images/sliderImages/moraine-lake-canada.jpeg';
+import Norway from '../assets/images/sliderImages/Myrland-Nordland-Norway.jpeg';
+import Bahamas from '../assets/images/sliderImages/Nassau-Nova-Providência-Bahamas.jpeg';
+import Paris from '../assets/images/sliderImages/paris-france.jpeg';
+import Brazil from '../assets/images/sliderImages/rio-de-janeiro-brazil.jpeg';
+import Rome from '../assets/images/sliderImages/Rome-Colloseum.jpeg';
+import Bautista from '../assets/images/sliderImages/San-Juan-Bautista.jpeg';
+import Greece from '../assets/images/sliderImages/Santorini-Greece.jpeg';
+import Mexico from '../assets/images/sliderImages/Tangier-morocco.jpeg';
+import Singapore from '../assets/images/sliderImages/singapore.jpeg';
+import Tokyo from '../assets/images/sliderImages/tokyo-japan-1.jpeg';
+import Venice from '../assets/images/sliderImages/Venice-Veneto-Italy.jpeg';
+import Wicklow from '../assets/images/sliderImages/Wicklow-Ireland.jpeg';
+import Poland from '../assets/images/sliderImages/Zakopane-poland.jpeg';
 
 
 const sliders = [
@@ -30,7 +29,7 @@ const sliders = [
     },
     {
         location: "Bermuda",
-        image: {Bermuda},
+        image: { Bermuda },
     },
     {
         location: "Central Province, Maldives",
