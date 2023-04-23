52249aa50c48957e1f24f0adb5c8a30a15637954
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-22 22:52:31 -0400

    error screen

--- front-end/src/pages/error.jsx
+++ front-end/src/pages/error.jsx
@@ -9,6 +9,7 @@ export default function Error() {
       <div className='flex flex-col items-center justify-center bg-dark-shade min-h-screen' id="error-page">
         <h1 className='p-8 text-2xl text-primary-light'>Oops!</h1>
         <p className='p-8 text-primary-light'>Sorry, an unexpected error has occurred.</p>
+        <h2>Still need help from baby blue skies? give us a call at </h2><a href="tel:+13179366685"><p className='p-8 text-primary-light text-xl'>+1(317)936-6685</p></a>
         <p className='p-8 text-primary-light'>
           <i>{error.statusText || error.message}</i>
         </p>
