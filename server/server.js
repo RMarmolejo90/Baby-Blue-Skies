dd2e6b3ac4cf2aa03fa1f409dd28203431fa0d78
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-24 07:52:43 -0400

    removed cors cred for server 404 error

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const emailController = require('./controllers/emailController');
 app.use(express.json());
 app.use(cors({
     origin: true,
-    credentials: true,
 }));
 // load env variables
 if (process.env.NODE_ENV != "production"){
@@ -30,22 +29,22 @@ app.post('/post', emailController.subscribe );
 // Delete (unsubscribe)
 app.delete('/unsubscribe', emailController.unsubscribe);
 
-// // Catch 404 errors
-// app.use((req, res, next) => {
-//     const error = new Error('Not found');
-//     error.status = 404;
-//     next(error);
-// });
+// Catch 404 errors
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
 
-// // Error handling middleware
-// app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     res.json({
-//         error: {
-//             message: error.message,
-//         },
-//     });
-// });
+// Error handling middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+});
 
 // Start Server
 app.get('/', (req, res) => {
