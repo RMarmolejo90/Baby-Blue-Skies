a17ccd960c541a293463efc3ed4381a81fbfc9ef
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-28 09:58:08 -0400

    fixed autoheight syntax

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -22,7 +22,7 @@ export default () => {
           modules={[Navigation, EffectCube, Autoplay]}
           enabled='true'
           autoplay={true}
-          autoheight={true}
+          autoheight
           effect='cube'
           loop='true'
           lazyPreloadPrevNext={2}
