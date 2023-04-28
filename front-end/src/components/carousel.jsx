a104eb0eb9a6c4af0be1b1766adc187bcfef5f83
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-04-28 09:36:36 -0400

    carousel style

--- front-end/src/components/carousel.jsx
+++ front-end/src/components/carousel.jsx
@@ -16,12 +16,13 @@ export default () => {
 
   return (
     <div className='py-20 bg-white-shade'>
-      <Fade duration={1000}>
+      <Fade duration={2000}>
         <Swiper
           // install Swiper modules
           modules={[Navigation, EffectCube, Autoplay]}
           enabled='true'
           autoplay={true}
+          autoheight={true}
           effect='cube'
           loop='true'
           lazyPreloadPrevNext={2}
@@ -29,14 +30,14 @@ export default () => {
           slidesPerView={1}
           navigation   
           onSlideChange={() => console.log('slide change')}
-          className=" max-w-sm lg:max-w-md shadow-2xl contrast-125 rounded-xl"
+          className=" max-w-xs lg:max-w-md"
         >
           {sliders.map( (sliders, index) => (
             <SwiperSlide key={index} className='slide'  >
               <div className='slide-wrapper'>
-              <img className='swiper-image overflow-hidden ' loading="lazy" src={sliders.image} alt={sliders.location} />
+              <img className='swiper-image min-h-full min-w-full overflow-hidden' loading="lazy" src={sliders.image} alt={sliders.location} />
                 <div className="swiper-lazy-preloader"></div>
-                <div className='text-white-shade max-h-sm lg:max-w-md font-bold p-4 text-2xl text-center bg-dark-shade'>
+                <div className='text-white-shade min-w-full lg:max-w-md font-bold p-4 text-2xl text-center bg-dark-shade'>
                   <h2 >{sliders.location}</h2>
                 </div> 
               </div>
--- server/models/emails.js
+++ server/models/emails.js
@@ -4,6 +4,8 @@ const { Schema } = mongoose;
 // Create Schema for emails
 const emailSchema = new Schema({email: String});
 
+console.log(emailSchema);
+
 const emailAddress = mongoose.model('emailAddress', emailSchema);
 
 module.exports = emailAddress;
